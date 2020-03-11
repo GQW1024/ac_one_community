06ad60349cd02a7ebfc01797d5c981575d34c920
GQW1024 <1367309971@qq.com>
2020-03-11 21:25:01 +0800

    处理好静态（static）资源的映射问题

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 依赖资源：https://spring.io/guides <br/>
 依赖资源：https://spring.io/guides/gs.serving-wed-content/ <br/>
 对标网站：https://elasticsearch.cn/explore <br/>
+使用git_hub账号进行标记：https://developer.github.com/apps/building-github-apps/creating-a-github-app/
 ## 工具 <br/>
 Git <br/>
 IntelliJ IDEA <br/>
--- src/main/java/ac_one/gqw1024/community/ac_one_community/AcOneCommunityApplication.java
+++ src/main/java/ac_one/gqw1024/community/ac_one_community/AcOneCommunityApplication.java
@@ -2,12 +2,26 @@ package ac_one.gqw1024.community.ac_one_community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class AcOneCommunityApplication {
+public class AcOneCommunityApplication extends WebMvcConfigurationSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(AcOneCommunityApplication.class, args);
     }
 
+    /**
+     * 配置在Spring启动时，扫描static下的所有文件，不然在导入css等文件时，会导入失败
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //静态文件的虚拟映射            //虚拟路径                                       //物理路径
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+//        //上传的文件的虚拟映射                    //虚拟路径                                  //物理路径
+//        registry.addResourceHandler("/uploadfile/**").addResourceLocations("file:D:/ACG_Package/");//使用方式，例:http://localhost:8080/uploadfile/Videoface/JoTaRo.jpeg
+        System.out.println("路径映射完毕");
+    }
 }
