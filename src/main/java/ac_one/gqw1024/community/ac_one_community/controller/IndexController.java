4a3be26cadae90a5c95bd95aa3722631df1a7a1e
GQW1024 <1367309971@qq.com>
2020-03-11 17:29:31 +0800

    导入BootStrap，新增index页面

---
+++ src/main/java/ac_one/gqw1024/community/ac_one_community/controller/IndexController.java
@@ -0,0 +1,16 @@
+package ac_one.gqw1024.community.ac_one_community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public ModelAndView meeting(@RequestParam(name = "name",required = false,defaultValue = "GQW") String name, ModelAndView modelAndView){
+        modelAndView.setViewName("index");
+        modelAndView.addObject("name",name);
+        return modelAndView;
+    }
+}
