ee8dee8e6d540ef9621b01a29db520dc88fc8aa2
GQW1024 <1367309971@qq.com>
2020-03-13 22:18:52 +0800

    2020_03_13_将主要变量配置到配置文件中去

--- src/main/java/ac_one/gqw1024/community/ac_one_community/controller/AuthorizeConntroller.java
+++ src/main/java/ac_one/gqw1024/community/ac_one_community/controller/AuthorizeConntroller.java
@@ -5,9 +5,12 @@ import ac_one.gqw1024.community.ac_one_community.dto.GithubUser;
 import ac_one.gqw1024.community.ac_one_community.provider.GithubProvider;
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import sun.net.www.http.HttpClient;
 
@@ -19,10 +22,18 @@ import java.util.concurrent.Executors;
  * Github用户信息处理Conntroller
  */
 @Controller
+@SessionAttributes({"user"})//将对应的用户信息存入session
 public class AuthorizeConntroller {
 
     @Autowired  //自动装配
     private GithubProvider githubProvider;//github用户信息提供者
+
+    @Value("${github.client.id}")   //中间填写的是配置文件中对应的key
+            String clientID; //Github上创建的 OAuth app 所持有的标识
+    @Value("${github.client.secret}")
+    String clientSecret; //Github上创建的 OAuth app 所持有的标识
+    @Value("${github.redirect.uri}")
+    String redirect_uri; //返回access_token的接口，/callback
     /**
      *
      * @param code  //authorize授权请求成功后，重定向回来时携带的数据，获取用户信息的步骤中所必要的数据
@@ -35,17 +46,19 @@ public class AuthorizeConntroller {
                                  @RequestParam(name = "state",required = false) String state,
                                  ModelAndView modelAndView) throws IOException {
         AccessTockenDTO tockenDTO = new AccessTockenDTO();
-        tockenDTO.setClient_id("68f471c8ef16a67e27ad"); //Github上创建的 OAuth app 所持有的标识
-        tockenDTO.setClient_secret("2a91283c97814ff165a6bd2de3a416ff65bcc4ff");  //Github上创建的 OAuth app 所持有的标识
+        tockenDTO.setClient_id(clientID); //Github上创建的 OAuth app 所持有的标识
+        tockenDTO.setClient_secret(clientSecret);  //Github上创建的 OAuth app 所持有的标识
         tockenDTO.setCode(code);
-        tockenDTO.setRedirect_uri("http://localhost:8080/callback");
+        tockenDTO.setRedirect_uri(redirect_uri);//返回access_token的接口，本方法
         tockenDTO.setState(state);
         String access_token =githubProvider.getAccessToken(tockenDTO);//获得返回的用户认证：access_token，并将其转为JSON字符串，方便使用        modelAndView.setViewName("index");
         //System.out.println(access_token);
         GithubUser user = githubProvider.getUser(access_token);//通过access_token向github获取用户信息
-        modelAndView.addObject("user",user);//将用户信息返回到前端
+        modelAndView.addObject("user",user);//将用户信息返回到前端，随后存入session
         modelAndView.setViewName("index");
         System.out.println(user.toString());
         return modelAndView;
     }
 }
+
+
