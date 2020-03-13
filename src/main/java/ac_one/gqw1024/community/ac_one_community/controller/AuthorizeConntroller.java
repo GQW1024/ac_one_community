d5fec368e95d6c597d39c843297cae8f32e8ed81
GQW1024 <1367309971@qq.com>
2020-03-13 21:17:22 +0800

    2020_03_13_实现从github获取用户信息

--- src/main/java/ac_one/gqw1024/community/ac_one_community/controller/AuthorizeConntroller.java
+++ src/main/java/ac_one/gqw1024/community/ac_one_community/controller/AuthorizeConntroller.java
@@ -1,7 +1,9 @@
 package ac_one.gqw1024.community.ac_one_community.controller;
 
 import ac_one.gqw1024.community.ac_one_community.dto.AccessTockenDTO;
+import ac_one.gqw1024.community.ac_one_community.dto.GithubUser;
 import ac_one.gqw1024.community.ac_one_community.provider.GithubProvider;
+import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import sun.net.www.http.HttpClient;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -30,10 +33,19 @@ public class AuthorizeConntroller {
     @RequestMapping("/callback")
     public ModelAndView collback(@RequestParam(name = "code",required = false) String code,
                                  @RequestParam(name = "state",required = false) String state,
-                                 ModelAndView modelAndView){
-
+                                 ModelAndView modelAndView) throws IOException {
+        AccessTockenDTO tockenDTO = new AccessTockenDTO();
+        tockenDTO.setClient_id("68f471c8ef16a67e27ad"); //Github上创建的 OAuth app 所持有的标识
+        tockenDTO.setClient_secret("2a91283c97814ff165a6bd2de3a416ff65bcc4ff");  //Github上创建的 OAuth app 所持有的标识
+        tockenDTO.setCode(code);
+        tockenDTO.setRedirect_uri("http://localhost:8080/callback");
+        tockenDTO.setState(state);
+        String access_token =githubProvider.getAccessToken(tockenDTO);//获得返回的用户认证：access_token，并将其转为JSON字符串，方便使用        modelAndView.setViewName("index");
+        //System.out.println(access_token);
+        GithubUser user = githubProvider.getUser(access_token);//通过access_token向github获取用户信息
+        modelAndView.addObject("user",user);//将用户信息返回到前端
         modelAndView.setViewName("index");
-        //modelAndView.addObject("name",name);
+        System.out.println(user.toString());
         return modelAndView;
     }
 }
--- src/main/java/ac_one/gqw1024/community/ac_one_community/provider/GithubProvider.java
+++ src/main/java/ac_one/gqw1024/community/ac_one_community/provider/GithubProvider.java
@@ -1,6 +1,7 @@
 package ac_one.gqw1024.community.ac_one_community.provider;
 
 import ac_one.gqw1024.community.ac_one_community.dto.AccessTockenDTO;
+import ac_one.gqw1024.community.ac_one_community.dto.GithubUser;
 import com.alibaba.fastjson.JSON;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
@@ -17,21 +18,46 @@ public class GithubProvider {
      * @throws IOException
      */
     public String getAccessToken(AccessTockenDTO accessTockenDTO) throws IOException{
-        MediaType json = MediaType.get("application/json; charset=utf-8");//请求体类型，以及编码格式
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");//请求体类型，以及编码格式
 
         OkHttpClient client = new OkHttpClient();
 
-            RequestBody body = RequestBody.create(json, JSON.toJSONString(accessTockenDTO));//创建请求体
+            RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTockenDTO));//创建请求体
             Request request = new Request.Builder()
                     .url("https://github.com/login/oauth/access_token")
                     .post(body)
                     .build();//创建请求
             try (Response response = client.newCall(request).execute()) {//发出请求
-                System.out.println(response.body().string());
-                return response.body().string();    //请求成功，返回信息
+                //response.body().string()的到的结果为access_token=...&scope=user&token_type=bearer
+                String access_tocken = response.body().string().split("&")[0].split("=")[1];  //通&字符截取字符串成数组{"access_token=...","scope=user","token_type=bearer"}
+                //String[] access_tocken = bodyarry[0].split("=");//由于bodyarry的第一个参数是我们所需要的access_tocken,所以用等号截取她
+                System.out.println(access_tocken);
+                return access_tocken;    //请求成功，截取完毕，返回access_tocken
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return null;   //请求失败，返回null
+            return null;   //如果请求失败，返回null
+    }
+
+    /**
+     *  用于获取github用户的个人信息，且只能使用get方式请求
+     * @param access_token  //【必要参数】获取用户信息的令牌（KEY）
+     * @return
+     */
+    public GithubUser getUser(String access_token){
+        //get请求发送步骤
+        OkHttpClient client = new OkHttpClient();// 1 创建 OkHttpClient
+        Request request = new Request.Builder() // 2 创建请求[Request]
+                .url("https://api.github.com/user?access_token="+access_token)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();// 3 利用OkHttpClient来将请求提交发送出去
+            //由于接收过来的已经是JSON格式的数据了，所以这里直接用parseObject()方法直接转为GithubUser类型返回。
+            return JSON.parseObject(response.body().string(),GithubUser.class);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;   //如果请求失败，返回null
     }
 }
